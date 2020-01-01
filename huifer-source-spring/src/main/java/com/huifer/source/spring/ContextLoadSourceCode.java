371982d5098b56b5e40f291bbcf74776ffa3c924
huifer <huifer97@163.com>
2020-01-01 16:27:26 +0800

    测试用例

--- huifer-source-spring/src/main/java/com/huifer/source/spring/ContextLoadSourceCode.java
+++ huifer-source-spring/src/main/java/com/huifer/source/spring/ContextLoadSourceCode.java
@@ -6,9 +6,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ContextLoadSourceCode {
     public static void main(String[] args) {
         // 这是一个最基本的 spring 调用的例子
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ContextLoadSourceCode-beans.xml");
         Person bean = context.getBean(Person.class);
         System.out.println(bean.getName());
         System.out.println(bean.getApple().getName());
+        bean.dis();
+        System.out.println(bean.getAge());
+
+
     }
 }

0a880a926c774c5c487a928f67aa5ed355786471
huifer <huifer97@163.com>
2020-01-02 09:58:55 +0800

    AliasRegistry

--- spring-core/src/main/java/org/springframework/core/AliasRegistry.java
+++ spring-core/src/main/java/org/springframework/core/AliasRegistry.java
@@ -27,16 +27,20 @@ public interface AliasRegistry {
 
     /**
      * Given a name, register an alias for it.
+     * 别名注册
      *
      * @param name  the canonical name
      * @param alias the alias to be registered
      * @throws IllegalStateException if the alias is already in use
      *                               and may not be overridden
+     * @see SimpleAliasRegistry
+     * @see org.springframework.context.support.GenericApplicationContext
      */
     void registerAlias(String name, String alias);
 
     /**
      * Remove the specified alias from this registry.
+     * 别名移除
      *
      * @param alias the alias to remove
      * @throws IllegalStateException if no such alias was found
@@ -46,6 +50,7 @@ public interface AliasRegistry {
     /**
      * Determine whether this given name is defines as an alias
      * (as opposed to the name of an actually registered component).
+     * 是不是别名
      *
      * @param name the name to check
      * @return whether the given name is an alias
@@ -54,6 +59,7 @@ public interface AliasRegistry {
 
     /**
      * Return the aliases for the given name, if defined.
+     * 从别名注册map中获取别名信息
      *
      * @param name the name to check for aliases
      * @return the aliases, or an empty array if none
